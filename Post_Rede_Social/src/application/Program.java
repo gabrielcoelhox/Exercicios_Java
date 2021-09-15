7f3a7536cf7be779d0e557e91a068548df913ae4
Gabriel Coelho <gabriel.coelhox@gmail.com>
2021-09-15 17:40:01 -0300

    add segundo post e stringbuilder para poder ler corretamente os posts

--- Post_Rede_Social/src/application/Program.java
+++ Post_Rede_Social/src/application/Program.java
@@ -3,7 +3,6 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
-import java.util.Scanner;
 
 import entities.Comment;
 import entities.Post;
@@ -13,7 +12,6 @@ public class Program {
 	public static void main(String[] args) throws ParseException {
 
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
@@ -26,8 +24,17 @@ public class Program {
 		p1.addComment(c1);
 		p1.addComment(c2);
 		
+		Comment c3 = new Comment("Good night");
+		Comment c4 = new Comment("May the Force with you");
+		Post p2 = new Post(sdf.parse("28/07/2018 23:14:19"),    // Moment
+				"Good night guys",                              // Title
+				"See you tomorrow",                             // Content
+				5);                                             // Likes
+		p2.addComment(c3);
+		p2.addComment(c4);
 		
-		sc.close();
+		System.out.println(p1);
+		System.out.println(p2);
 	}
 
 }
