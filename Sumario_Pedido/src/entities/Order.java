5160200cf7ca628754f2fdfd866e3d246ac144b6
Gabriel Coelho <gabriel.coelhox@gmail.com>
2021-09-16 17:20:08 -0300

    add toString para formatar

--- Sumario_Pedido/src/entities/Order.java
+++ Sumario_Pedido/src/entities/Order.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -10,6 +11,8 @@ public class Order {
 
 	private Date moment;
 	private OrderStatus status;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	
 	private Client client;
 	
@@ -61,5 +64,24 @@ public class Order {
 		for (OrderItem it: items) {
 			sum += it.subTotal();
 		}
+		return sum;
 	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Order moment: ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append("Order Status: ");
+		sb.append(status + "\n");
+		sb.append("Client: ");
+		sb.append(client + "\n");
+		sb.append("Order items:\n");
+		for (OrderItem item : items) {
+			sb.append(item + "\n");
+		}
+		sb.append("Total price: $");
+		sb.append(String.format("%.2f" ,total()));
+		return sb.toString();
+	}
+	
 }
