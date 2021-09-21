efeec073a3d43feb25bcb41b8d72a9d722942c16
Gabriel Coelho <gabriel.coelhox@gmail.com>
2021-09-21 17:00:05 -0300

    Finalizando exercício

--- Etiqueta_Alfandega/src/application/Program.java
+++ Etiqueta_Alfandega/src/application/Program.java
@@ -13,19 +13,26 @@ do produto.
 
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.ImportedProduct;
 import entities.Product;
+import entities.UsedProduct;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		List<Product> list = new ArrayList<>();
 		
@@ -37,6 +44,31 @@ public class Program {
 			System.out.print("Common, used or imported (c/u/i)? ");
 			char type = sc.next().charAt(0);
 			
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.next();
+			System.out.print("Price: ");
+			Double price = sc.nextDouble();
+			
+			if (type == 'c' || type == 'C') {
+				list.add(new Product(name, price));
+			} else if (type == 'u' || type == 'U'){
+				System.out.print("Manufacture date (DD/MM/YYYY): ");
+				Date date = sdf.parse(sc.next());
+				list.add(new UsedProduct(name, price, date));
+			} else {
+				System.out.print("Customs fee: ");
+				Double fee = sc.nextDouble();
+				list.add(new ImportedProduct(name, price, fee));
+			}
+			
+			System.out.println();
+			System.out.println("* PRICE TAGS *");
+			for (Product prod : list) {
+				System.out.println(prod.priceTag());
+			}
+			
+			sc.close();
 		}
 
 	}
