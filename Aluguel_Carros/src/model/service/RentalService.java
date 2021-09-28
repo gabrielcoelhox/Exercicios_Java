0c5a77d13e9f48ca88fa88d22204440f76a560e9
Gabriel Coelho <gabriel.coelhox@gmail.com>
2021-09-28 20:46:11 -0300

    add exercício aluguel de carros

---
+++ Aluguel_Carros/src/model/service/RentalService.java
@@ -0,0 +1,36 @@
+package model.service;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+
+	private Double pricePerHour;
+	private Double pricePerDay;
+	
+	private BrazilTaxService taxService;
+	
+	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+		this.taxService = taxService;
+	}
+
+	public void processInvoice(CarRental carRental) {
+		long time1 = carRental.getStart().getTime();
+		long time2 = carRental.getFinish().getTime();
+		double hours = (double)(time2 - time1) / 1000 / 60 / 60;    // Segundos, minutos e horas
+		
+		double basicPayment;
+		if (hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		} else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+	
+}
